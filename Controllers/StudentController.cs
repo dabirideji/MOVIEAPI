906b377f9f795844b1b581b3fdd57dbac327ad9d
dabirideji <115591934+dabirideji@users.noreply.github.com>
2023-09-23 14:47:18 +0100

    implemented jwt Authentication

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,94 +1,122 @@
+using System.Security.Claims;
 using APICLASS.Models;
 using APICLASS.Response;
 using BCrypt.Net;
+// using System.IdentityModel.Token;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MOVIEAPI.Data;
 using MOVIEAPI.Models;
 using SQLitePCL;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection.Metadata.Ecma335;
 
-namespace MOVIEAPI.Controllers{
-[ApiController]
-[Route("api/[controller]")]
-public class StudentController:ControllerBase
+namespace MOVIEAPI.Controllers
 {
-    private readonly MovieDbContext _context;
-
-    private readonly ILogger _logger;
-    public StudentController(MovieDbContext context, ILogger<StudentController> logger)
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
     {
-        _context = context;
-        _logger = logger;
-    }
+        private readonly MovieDbContext _context;
 
-
-    [HttpPost]
-    [Route("login")]
-    public ActionResult<DefaultResponse<Student>> StudentLogin([FromBody]StudentLoginDto dto){
-        DefaultResponse<Student> response=new();
-        // var result=await _context.Students.Where(x=>x.Username==dto.Username&&BCrypt.Net.BCrypt.Verify(dto.Password,x.Password)).ToListAsync();
-        var result=_context.Students.Where(x=>x.Username==dto.Username).FirstOrDefault();
-        if(result==null){
-            return BadRequest("USER NOT FOUND");
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+        public StudentController(MovieDbContext context, ILogger<StudentController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _context = context;
+            _logger = logger;
         }
-       if(!BCrypt.Net.BCrypt.Verify(dto.Password,result.Password)){
-            return BadRequest("WRONG PASSWORD");
 
-        }
-      
-         response.Status = true;
+
+        [HttpPost]
+        [Route("login")]
+        public ActionResult<DefaultResponse<Student>> StudentLogin([FromBody] StudentLoginDto dto)
+        {
+            DefaultResponse<Student> response = new();
+            // var result=await _context.Students.Where(x=>x.Username==dto.Username&&BCrypt.Net.BCrypt.Verify(dto.Password,x.Password)).ToListAsync();
+            var result = _context.Students.Where(x => x.Username == dto.Username).FirstOrDefault();
+            if (result == null)
+            {
+                return BadRequest("USER NOT FOUND");
+            }
+            if (!BCrypt.Net.BCrypt.Verify(dto.Password, result.Password))
+            {
+                return BadRequest("WRONG PASSWORD");
+
+            }
+            string token = GenerateToken(result);
+            response.Status = true;
             response.ResponseMessage = "Student Found Successfully";
             response.ResponseCode = "00";
             response.Data = result;
-            _logger.LogInformation("Student Found Successfully");
-        return Ok(response);
-    }
-
-    [HttpPost]
-    [Route("add")]
-    public async Task<ActionResult<DefaultResponse<Movie>>> AddStudent([FromBody] StudentDto student)
-    {
-        string passwordHash=BCrypt.Net.BCrypt.HashPassword(student.Password);
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
+            _logger.LogInformation("Student Login Successfully: " + token);
+            return Ok(response);
         }
 
-        DefaultResponse<string> response = new();
-        try
+
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<ActionResult<DefaultResponse<Movie>>> AddStudent([FromBody] StudentDto student)
         {
-            var stud = new Student()
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(student.Password);
+            if (!ModelState.IsValid)
             {
-                Name = student.Name,
-                Username = student.Username,
-                Password = passwordHash,
+                return BadRequest(ModelState);
+            }
+
+            DefaultResponse<string> response = new();
+            try
+            {
+                var stud = new Student()
+                {
+                    Name = student.Name,
+                    Username = student.Username,
+                    Password = passwordHash,
+                };
+                await _context.Students.AddAsync(stud);
+                await _context.SaveChangesAsync();
+                response.Status = true;
+                response.ResponseMessage = "Data Saved Successfully";
+                response.ResponseCode = "00";
+                // response.Data = response.ResponseMessage;
+                _logger.LogInformation("Data Saved Successfully");
+
+                return Ok(stud);
+                // response.Data = movie;
+                // return StatusCode(201,response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.ResponseMessage = "An Error Occuured";
+                response.ResponseCode = "99";
+                _logger.LogError("An Error Occured", ex);
+                return StatusCode(500, response);
             };
-            await _context.Students.AddAsync(stud);
-            await _context.SaveChangesAsync();
-            response.Status = true;
-            response.ResponseMessage = "Data Saved Successfully";
-            response.ResponseCode = "00";
-            // response.Data = response.ResponseMessage;
-            _logger.LogInformation("Data Saved Successfully");
 
-            return Ok(stud);
-            // response.Data = movie;
-            // return StatusCode(201,response);
+
+
         }
-        catch (Exception ex)
+
+        private string GenerateToken(Student stu)
         {
-            response.Status = false;
-            response.ResponseMessage = "An Error Occuured";
-            response.ResponseCode = "99";
-            _logger.LogError("An Error Occured", ex);
-            return StatusCode(500, response);
-        };
-
-
-
+            List<Claim> claims = new List<Claim>{
+        new Claim(ClaimTypes.Name,stu.Username)
+    };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("JWT:TOKEN").Value!));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: cred
+            );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
+        }
     }
-
-
-}
 
 }
